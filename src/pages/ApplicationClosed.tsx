3d280695fa2141ea970cb8b2bdefcc87dbcfdee6
Godspower Similoluwa <131388888+gpsimi@users.noreply.github.com>
2025-12-26 16:46:10 +0100

    Update ApplicationClosed.tsx

--- src/pages/ApplicationClosed.tsx
+++ src/pages/ApplicationClosed.tsx
@@ -1,21 +1,33 @@
 import { Button } from "@/components/ui/button";
+import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import Navbar from "@/components/Navbar";
-import ApplicationClosedImage from "@/assets/application-2.png"; // Import the image
+import ApplicationClosedImage from "@/assets/application-2.png";
+
 
 const ApplicationClosed = () => {
   return (
     <>
       <Navbar />
       <div className="flex flex-col md:flex-row items-center justify-center h-[95vh]  px-4 py-10 md:space-x-24">
-        <div className="flex-shrink-0 mb-8 md:mb-0">
+        <motion.div
+          className="flex-shrink-0 mb-8 md:mb-0"
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5 }}
+        >
           <img
             src={ApplicationClosedImage}
             alt="Applications Closed"
-            className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-md h-auto"
+            className="rounded-2xl shadow-xl w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-md h-auto"
           />
-        </div>
-        <div className="text-center md:text-left">
+        </motion.div>
+        <motion.div
+          className="text-center md:text-left"
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+        >
           <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-100 mb-4">
             Applications Closed!
           </h1>
@@ -27,8 +39,8 @@ const ApplicationClosed = () => {
               Go to Home Page
             </Button>
           </Link>
-        </div>
-      </div>
+      </motion.div>
+    </div >
     </>
   );
 };
